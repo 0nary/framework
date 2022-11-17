18f71e1cfcc490c2e98eaabd6d58c8d978055a02
Gregoire Poncelet <gregoire.poncelet@neuroglia.io>
2022-11-17 14:56:38 +0100

    test(RazorLightTemplateRendererTests): change renderer

--- test/Neuroglia.UnitTests/Cases/Templating/RazorLightTemplateRendererTests.cs
+++ test/Neuroglia.UnitTests/Cases/Templating/RazorLightTemplateRendererTests.cs
@@ -4,9 +4,14 @@ using Microsoft.Extensions.DependencyInjection;
 using Neuroglia.Templating;
 using Neuroglia.Templating.Services;
 using Neuroglia.UnitTests.Cases.Data;
+using RazorLight;
 using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
 using System.Threading.Tasks;
 using Xunit;
+using Xunit.Sdk;
 
 namespace Neuroglia.UnitTests.Cases.Templating
 {
@@ -33,9 +38,18 @@ namespace Neuroglia.UnitTests.Cases.Templating
         public async Task RenderTemplate()
         {
             //arrange
+            var engine = new RazorLightEngineBuilder()
+            // required to have a default RazorLightProject type,
+            // but not required to create a template from string.
+                .UseEmbeddedResourcesProject(typeof(RazorLightTemplateRendererTests))
+                 .SetOperatingAssembly(typeof(RazorLightTemplateRendererTests).Assembly)
+                .UseMemoryCachingProvider()
+                .Build();
+
             var model = new TestModel();
             var template = @"
     @model Neuroglia.UnitTests.Cases.Data.TestModel;
+
     <select>
         @foreach(string option in this.Model.Options)
         {
@@ -45,7 +59,10 @@ namespace Neuroglia.UnitTests.Cases.Templating
 ";
 
             //act
-            var rendered = await this.TemplateRenderer.RenderTemplateAsync(template, model);
+            //var rendered = await this.TemplateRenderer.RenderTemplateAsync(template, model);
+            using MemoryStream stream = new(Encoding.UTF8.GetBytes(template));
+            string hash = Encoding.UTF8.GetString(await MD5.Create().ComputeHashAsync(stream));
+            var rendered = await engine.CompileRenderStringAsync(hash, template, model);
 
             //assert
             rendered.Should().NotBeNullOrWhiteSpace();

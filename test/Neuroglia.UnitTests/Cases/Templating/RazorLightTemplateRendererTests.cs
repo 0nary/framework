67c92fc0d5cd05b3a7eb28faec228cac0b83519b
Gregoire Poncelet <gregoire.poncelet@neuroglia.io>
2022-11-17 12:16:39 +0100

    test(RazorLightTemplateRendererTests): test first call

--- test/Neuroglia.UnitTests/Cases/Templating/RazorLightTemplateRendererTests.cs
+++ test/Neuroglia.UnitTests/Cases/Templating/RazorLightTemplateRendererTests.cs
@@ -1,4 +1,5 @@
 ﻿using FluentAssertions;
+using Google.Protobuf.WellKnownTypes;
 using Microsoft.Extensions.DependencyInjection;
 using Neuroglia.Templating;
 using Neuroglia.Templating.Services;
@@ -35,8 +36,13 @@ namespace Neuroglia.UnitTests.Cases.Templating
             var model = new TestModel();
             var template = @"
     @model Neuroglia.UnitTests.Cases.Data.TestModel;
+    @{
+        string test = this.Model.Options[0];
+    }
+    <option>@test</option>
+
     <select>
-        @foreach(string option in this.Model?.Options)
+        @foreach(string option in this.Model.Options)
         {
             <option value=""@option"">@option</option>
         }
@@ -48,6 +54,7 @@ namespace Neuroglia.UnitTests.Cases.Templating
 
             //assert
             rendered.Should().NotBeNullOrWhiteSpace();
+            rendered.Should().Contain("<option>option1</option>");
             rendered.Should().Contain("<select>");
             rendered.Should().Contain($"<option value=\"{model.Options[0]}\">{model.Options[0]}</option>");
             rendered.Should().Contain($"<option value=\"{model.Options[1]}\">{model.Options[1]}</option>");

7bef300010c83f2a19d7b55f04fc5262465b4e49
Poncelet Grégoire <gregoire.poncelet@neuroglia.io>
2022-11-18 13:22:09 +0100

    test(repositories): dispose DbContext

--- test/Neuroglia.UnitTests/Cases/Data/Repositories/EFCoreRepositoryTests.cs
+++ test/Neuroglia.UnitTests/Cases/Data/Repositories/EFCoreRepositoryTests.cs
@@ -26,7 +26,7 @@ namespace Neuroglia.UnitTests.Cases.Data.Repositories
             AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
             this.ServiceScope = services.BuildServiceProvider().CreateScope();
             this.DbContext = this.ServiceScope.ServiceProvider.GetRequiredService<TestDbContext>();
-            //this.Repository = this.ServiceScope.ServiceProvider.GetRequiredService<EFCoreRepository<TestPerson, Guid, TestDbContext>>();
+            this.Repository = this.ServiceScope.ServiceProvider.GetRequiredService<EFCoreRepository<TestPerson, Guid, TestDbContext>>();
         }
 
         IServiceScope ServiceScope { get; }
@@ -146,6 +146,7 @@ namespace Neuroglia.UnitTests.Cases.Data.Repositories
         public void Dispose()
         {
             this.ServiceScope.Dispose();
+            this.DbContext.Dispose();
         }
 
     }
